7fe765cd90f1306a49ed5052d939aa2abd698c8b
Soham Goswami <sohamgoswami55@gmail.com>
2023-02-20 16:56:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const getNewMeme = () => {
   fetch("https://meme-api.com/gimme")
     .then((response) => response.json())
     .then((json) => {
-      dogImageDiv.innerHTML = `<img src="${json.url}" height="450" width="450">`;
+      dogImageDiv.innerHTML = `<img src="${json.url}" height="400" width="400">`;
     });
 };
 
