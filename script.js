0c1d96614d4728e96cbc04043d462f39008010e4
Soham Goswami <sohamgoswami55@gmail.com>
2023-02-20 16:48:36 +0530

    Added files for meme generator

---
+++ script.js
@@ -0,0 +1,12 @@
+const dogButton = document.getElementById("memeButton");
+const dogImageDiv = document.getElementById("memeImage");
+
+const getNewMeme = () => {
+  fetch("https://meme-api.com/gimme")
+    .then((response) => response.json())
+    .then((json) => {
+      dogImageDiv.innerHTML = `<img src="${json.url}" height="500" width="500">`;
+    });
+};
+
+dogButton.onclick = () => getNewMeme();
